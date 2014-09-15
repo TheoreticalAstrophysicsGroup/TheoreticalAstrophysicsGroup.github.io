bed13247676fbe46e0dd40b6bed06604b50b9afc
ccsastro <astro.ccs.tsukuba@gmail.com>
2014-09-15 05:48:52 +0000

    Updating to TheoreticalAstrophysicsGroup/TheoreticalAstrophysicsGroup.github.io@79c11c46bcb6a852e9299b3bb442f07730037302.

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-#source 'http://production.cf.rubygems.org/'
-
-gem 'rake'
-gem 'github-pages'
-
---
+++ summat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#===============================================================================
+#          FILE:  summat.sh
+#         USAGE:  ./summat.sh 
+#   DESCRIPTION:  Just a background script that outputs "summat" at 
+#                 regular intervals
+##===============================================================================
+
+n=1; while [[ $n -lt 51 ]]; do echo summat; sleep 60; ((n++)); done &
+
