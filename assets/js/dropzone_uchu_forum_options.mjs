36ed4e51b690d0a599ecbce5f63db243ed9ca7a0
Alex Y. Wagner <alexander.y.wagner@gmail.com>
2022-05-18 08:51:57 +0900

    Dropzone works but...
    It's still messy, and file-upload needs to be separated from rest of form.

---
+++ assets/js/dropzone_uchu_forum_options.mjs
@@ -0,0 +1,22 @@
+export function uf_options() {
+
+  var options;
+  options = {
+    maxFilesize: 30, // MB
+    uploadMultiple: true,
+    parallelUploads: 4,
+    maxFiles: 4,
+    acceptedFiles: "image/png,image/jpeg", 
+    addRemoveLinks: true,
+    dictDefaultMessage: 'Drag &amp; drop <i class="fa fa-cloud-upload fa-5x" aria-hidden="true"></i> or click here',
+    dictInvalidFileType: "File type unsupported.",
+    dictFileTooBig: "File too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+    dictMaxFilesExceeded: "File upload limit reached.",
+    dictCancelUpload: "Cancel upload",
+    dictCancelUploadConfirmation: "Cancel upload?"
+  };
+
+  return options;
+
+}
+
