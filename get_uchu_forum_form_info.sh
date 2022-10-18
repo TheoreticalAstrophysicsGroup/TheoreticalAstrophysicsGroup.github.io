9aa9b582d1de78fcd9da7ba125afb7425a95dd88
Alex Y. Wagner <alexander.y.wagner@gmail.com>
2022-10-18 15:36:41 +0900

    Add a little bit of error checking to sync script

--- get_uchu_forum_form_info.sh
+++ get_uchu_forum_form_info.sh
@@ -1,4 +1,13 @@
-# Choose "seminars" or "uchu_forum" as required first argument adn additional rsync options (e.g. -n) as optional second argument.
+# Choose 
+# 1) "seminars" or "uchu_forum" as required first argument 
+# 2) additional rsync options (e.g. -n) as optional second argument
 
-rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/img/ assets/img/$1/
-rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/yml/ $1/_posts/
+if [[ "$1" == "uchu_forum" ]] || [[ "$i" == "seminars" ]]; then
+  rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/img/ assets/img/$1/
+  rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/yml/ $1/_posts/
+
+else
+  echo "Error: Invalid first parameter."
+
+fi
+
