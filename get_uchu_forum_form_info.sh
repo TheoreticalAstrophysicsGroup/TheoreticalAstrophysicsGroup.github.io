74902f646af72f3882c5c5a7defe3ddfbd90806c
Alex Y. Wagner <alexander.y.wagner@gmail.com>
2022-12-06 15:57:33 +0900

    Important Uchu forum form fix
    Where a stray required existed in the Surname field, even when form was in English.

--- get_uchu_forum_form_info.sh
+++ get_uchu_forum_form_info.sh
@@ -2,7 +2,7 @@
 # 1) "seminars" or "uchu_forum" as required first argument 
 # 2) additional rsync options (e.g. -n) as optional second argument
 
-if [[ "$1" == "uchu_forum" ]] || [[ "$i" == "seminars" ]]; then
+if [[ "$1" == "uchu_forum" ]] || [[ "$1" == "seminars" ]]; then
   rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/img/ assets/img/$1/
   rsync -Pavi $2 charon.ccs.tsukuba.ac.jp:~/Astro/uchu_forum/uchu_forum_form/yml/ $1/_posts/
 
